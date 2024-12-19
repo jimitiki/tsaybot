18bfaedbd02a7bc2dfd5ee3fcc2a6fb66646efea
Chris Johnson <dev@cwjohnson.net>
2024-12-19 12:05:30 -0800

    Send discord.py logs to a file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from bot import Bot
 
@@ -6,6 +7,7 @@ with open ('./config.json') as config_file:
 	env = json.load(config_file)['default']
 bot = Bot(env['server_id'], env['vote_channel_id'], env['announce_channel_id'])
 
+log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
 with open('./token.txt') as token_file:
 	token = token_file.read().strip()
-bot.run(token)
+bot.run(token, log_handler=log_handler)

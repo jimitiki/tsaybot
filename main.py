f7464ce95be8eb801f6e7d46a7985f5324db8c5e
jimitiki <dev@jimitiki.com>
2024-12-19 11:15:46 -0800

    Move server and channel IDs to a config file

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# import logging
+import json
 
 from bot import Bot
 
-with open( './token.txt' ) as token_file:
+with open ('./config.json') as config_file:
+	env = json.load(config_file)['default']
+bot = Bot(env['server_id'], env['vote_channel_id'], env['announce_channel_id'])
+
+with open('./token.txt') as token_file:
 	token = token_file.read().strip()
-
-guild_id = 329842858909499392
-vote_channel_id = 660970005248344085
-announce_channel_id = 1298875448133943348
-
-bot = Bot(guild_id, vote_channel_id, announce_channel_id)
 bot.run(token)

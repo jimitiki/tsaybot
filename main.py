153c5193c0b48d1311d1a569681fef468afc59fa
jimitiki <dev@jimitiki.com>
2025-06-19 02:19:09 -0400

    Parametrize the token file path

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ import click
 from bot import Bot
 
 @click.command()
-def main(config_path: io.TextIOWrapper):
 @click.option('--config', 'config_file', envvar='TSAYBOT_CONFIG_PATH', default='config.json', type=click.File())
+@click.option('--token', 'token_file', envvar='TSAYBOT_DISCORD_TOKEN_PATH', default='token.txt', type=click.File())
 def main(config_file: io.TextIOWrapper):
 
 	config = json.load(config_file)
 	bot = Bot(config['server'], config['vote_channel'], config['announce_channel'], config['voice_channel'], config['role'])
 
 	log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
-	with open('token.txt') as token_file:
-		token = token_file.read().strip()
+	token = token_file.read().strip()
 	bot.run(token, log_handler=log_handler)
 
 if __name__ == '__main__':

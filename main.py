151a254de813b5a4411a2900750b7abc0f309e55
jimitiki <dev@jimitiki.com>
2025-06-19 02:28:33 -0400

    Switch to TOML config format

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import io
-import json
 import logging
+import tomllib
 
 import click
 
 from bot import Bot
 
 @click.command()
-@click.option('--config', 'config_file', envvar='TSAYBOT_CONFIG_PATH', default='config.json', type=click.File())
+@click.option('--config', 'config_file', envvar='TSAYBOT_CONFIG_PATH', default='config.toml', type=click.File(mode='rb'))
 @click.option('--token', 'token_file', envvar='TSAYBOT_DISCORD_TOKEN_PATH', default='token.txt', type=click.File())
-def main(config_file: io.TextIOWrapper):
+def main(config_file: io.BufferedReader, token_file: io.TextIOWrapper):
 
-	config = json.load(config_file)
-	bot = Bot(config['server'], config['vote_channel'], config['announce_channel'], config['voice_channel'], config['role'])
+	config = tomllib.load(config_file)
+	discord_cfg = config['discord']
+	bot = Bot(discord_cfg['server'], discord_cfg['vote_channel'], discord_cfg['announce_channel'], discord_cfg['voice_channel'], discord_cfg['role'])
 
 	log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
 	token = token_file.read().strip()

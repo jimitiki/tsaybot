6e30f749fbe3f2f6345dbc5eb4684cfa4f76fd8d
jimitiki <dev@jimitiki.com>
2025-06-19 13:19:46 -0400

    Make various paths configurable in TOML config

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ from bot import Bot, logger as bot_logger
 
 @click.command()
 @click.option('--config', 'config_file', envvar='TSAYBOT_CONFIG_PATH', default='config.toml', type=click.File(mode='rb'))
-@click.option('--token', 'token_file', envvar='TSAYBOT_DISCORD_TOKEN_PATH', default='token.txt', type=click.File())
-@click.option('--logdir', 'logs_dir', envvar='TSAYBOT_LOGS_DIR', default='./logs', type=click.Path(
+@click.option('--token', 'token_file', envvar='TSAYBOT_DISCORD_TOKEN_PATH', type=click.File())
+@click.option('--logdir', 'logs_dir', envvar='TSAYBOT_LOGS_DIR', type=click.Path(
 	file_okay=False, writable=True, executable=True, resolve_path=True, path_type=pathlib.Path
 ))
-@click.option('--eventdir', 'events_dir', envvar='TSAYBOT_EVENTS_DIR', default='./events', type=click.Path(
+@click.option('--eventdir', 'events_dir', envvar='TSAYBOT_EVENTS_DIR', type=click.Path(
 	file_okay=False, writable=True, executable=True, resolve_path=True, path_type=pathlib.Path
 ))
-def main(config_file: io.BufferedReader, token_file: io.TextIOWrapper, logs_dir: pathlib.Path, events_dir: pathlib.Path):
+def main(
+	config_file: io.BufferedReader,
+	token_file: io.TextIOWrapper|None,
+	logs_dir: pathlib.Path|None,
+	events_dir: pathlib.Path|None
+):
 
 	config = tomllib.load(config_file)
+	paths_cfg = config.get('paths', {})
+	events_dir = events_dir or pathlib.Path(paths_cfg.get('events_dir') or './events')
+	logs_dir = logs_dir or pathlib.Path(paths_cfg.get('logs_dir') or './logs')
 	os.makedirs(events_dir, mode=0o755, exist_ok=True)
-	discord_cfg = config['discord']
+	os.makedirs(logs_dir, mode=0o755, exist_ok=True)
+
+	discord_cfg = config.get('discord', {})
 	bot = Bot(
 		discord_cfg['server'],
 		discord_cfg['vote_channel'],
@@ -31,14 +41,18 @@ def main(config_file: io.BufferedReader, token_file: io.TextIOWrapper, logs_dir:
 		events_dir,
 	)
 
-	os.makedirs(logs_dir, mode=0o755, exist_ok=True)
 	log_handler = logging.FileHandler(logs_dir / 'discord.log')	# Logs exclusively emitted by the discord.py library
 	bot_logger.setLevel(logging.DEBUG)
 	handler = logging.FileHandler(logs_dir / 'bot.log')
 	formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {message} (task: {taskName})', style = '{')
 	handler.setFormatter(formatter)
 	bot_logger.addHandler(handler)
+
+	if not token_file:
+		token_file = open(paths_cfg.get('token', './token.txt'), 'r')
 	token = token_file.read().strip()
+	token_file.close()
+
 	bot.run(token, log_handler=log_handler)
 
 if __name__ == '__main__':

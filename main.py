b0004afbb21793417e2c730c42b8e2c3398f0a3e
jimitiki <dev@jimitiki.com>
2025-06-19 02:03:51 -0400

    Wrap startup script in `click` command

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import json
 import logging
 
+import click
+
 from bot import Bot
 
-with open ('./config.json') as config_file:
-	env = json.load(config_file)['default']
-bot = Bot(env['server'], env['vote_channel'], env['announce_channel'], env['voice_channel'], env['role'])
+@click.command()
+def main():
 
-log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
-with open('./token.txt') as token_file:
-	token = token_file.read().strip()
-bot.run(token, log_handler=log_handler)
+	with open('./config.json') as config_file:
+		env = json.load(config_file)['default']
+	bot = Bot(env['server'], env['vote_channel'], env['announce_channel'], env['voice_channel'], env['role'])
+
+	log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
+	with open('token.txt') as token_file:
+		token = token_file.read().strip()
+	bot.run(token, log_handler=log_handler)
+
+if __name__ == '__main__':
+
+	main()

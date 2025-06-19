17f5948cf61ab57fa3a6e946caebec8f1d56e8d2
jimitiki <dev@jimitiki.com>
2025-06-19 08:57:27 -0400

    Parametrize the logs directory
    This has the side effect of, in most cases, keeping all logs in a
    separate directory from other program files.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import io
 import logging
+import os
+import pathlib
 import tomllib
 
 import click
@@ -9,15 +11,19 @@ from bot import Bot, logger as bot_logger
 @click.command()
 @click.option('--config', 'config_file', envvar='TSAYBOT_CONFIG_PATH', default='config.toml', type=click.File(mode='rb'))
 @click.option('--token', 'token_file', envvar='TSAYBOT_DISCORD_TOKEN_PATH', default='token.txt', type=click.File())
-def main(config_file: io.BufferedReader, token_file: io.TextIOWrapper):
+@click.option('--logdir', 'logs_dir', envvar='TSAYBOT_LOGS_DIR', default='./logs', type=click.Path(
+	file_okay=False, writable=True, executable=True, resolve_path=True, path_type=pathlib.Path
+))
+def main(config_file: io.BufferedReader, token_file: io.TextIOWrapper, logs_dir: pathlib.Path):
 
 	config = tomllib.load(config_file)
 	discord_cfg = config['discord']
 	bot = Bot(discord_cfg['server'], discord_cfg['vote_channel'], discord_cfg['announce_channel'], discord_cfg['voice_channel'], discord_cfg['role'])
 
-	log_handler = logging.FileHandler('discord.log')	# Logs exclusively emitted by the discord.py library
+	os.makedirs(logs_dir, mode=0o755, exist_ok=True)
+	log_handler = logging.FileHandler(logs_dir / 'discord.log')	# Logs exclusively emitted by the discord.py library
 	bot_logger.setLevel(logging.DEBUG)
-	handler = logging.FileHandler('bot.log')
+	handler = logging.FileHandler(logs_dir / 'bot.log')
 	formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {message} (task: {taskName})', style = '{')
 	handler.setFormatter(formatter)
 	bot_logger.addHandler(handler)

3d17b38e3a7330824322df08070e3d2012af5d33
jimitiki <dev@jimitiki.com>
2025-06-23 08:44:30 -0400

    Remove `find_emoji`; emoji library dependency

--- scanner.py
+++ scanner.py
@@ -1,26 +1,6 @@
-from typing import Generator
-
-from discord import PartialEmoji
-import emoji
 import regex
 
-EMOJI_PATTERN = regex.compile(r'<a?:[^:]+:\d*>|\X')
 URL_PATTERN = regex.compile(r'^https://(boxd\.it|letterboxd\.com)[^\s]*$')
-
-def find_emoji(s: str) -> Generator[PartialEmoji,None,None]:
-	"""
-	Yields all unicode and custom Discord emojis in the string.
-
-	Emojis are yielded in the same order which they occur.
-	"""
-
-	for grapheme in EMOJI_PATTERN.findall(s):
-		if (
-			grapheme.startswith('<') and grapheme.endswith('>')		# Custom emoji in the form of "<a:name:id>" where "a" is present if animated and absent if not.
-			or emoji.is_emoji(grapheme)								# Standard unicode emoji
-			or ord(grapheme) in range(0x0001F1E6, 0x0001F200)		# Regional indicators (🇦-🇿)
-		):
-			yield PartialEmoji.from_str(grapheme)
 
 def is_url(s: str) -> bool:
 	"""
@@ -28,7 +8,3 @@ def is_url(s: str) -> bool:
 	"""
 
 	return bool(URL_PATTERN.match(s))
-
-if __name__ == '__main__':
-
-	print(list(find_emoji("<:shyguy:1319335606878470164> 😀 🇨🇨 🇺🇾 ⚡ 🇦 🇺 🇿 😈 👩‍⚖️ ✍️ 🧎🏻‍➡️ 🧑🏼‍🎓 🙆🏾‍♀️ 🧑🏿‍🎨")))
